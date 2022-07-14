62c403d4e221a9ab25232f71ce01324c8f89d815
DarkShiny <69814650+xDarkShiny@users.noreply.github.com>
2022-07-14 13:02:30 +0300

    Lastest Version

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,43 +1,36 @@
 <script>
 import { each } from "svelte/internal";
 import autoAnimate from "@formkit/auto-animate"
+import { toast } from '@zerodevx/svelte-toast'
+import { nanoid } from "nanoid";
 
 
 	let todos = [
-		{ name: "Bisiklete Çık", id: "1", complete: false},
-		{ name: "Valo Oyna Ark Sat", id: "2", complete: true},
-		{ name: "İlaçlarını Al", id: "3", complete: true}
+		{ title: "Bisiklete Çık", id: nanoid(), complete: false},
+		{ title: "Oyun Oyna",  id: nanoid(), complete: true},
+		{ title: "İlaçlarını Al",  id: nanoid(), complete: true}
 	];
-	let name2 = "";
-	
 
-	const addTodo = () => {
-		let todo = {
-		name: name2,
-		id: todos.length + 1,
-		complete: false
+	let title2 = "";
+
+	const addTodo = (title) => {
+		todos = [ ...todos,
+			{ title: title2 , id: nanoid(), num: todos.length + 1, complete: false } ];
 	};
-		todos = [...todos, todo];
-		name2 = ""
+
+	const deleteTodo = (id) => {
+		todos = todos.filter(todo => todo.id !== id);
+	};
+
+	const completeTodo = (id) => {
+		todos = todos.map(todo => {
+			if (todo.id === id) {
+				todo.complete = !todo.complete;
+			}
+			return todo;
+		});
+	};
 		
-	};
-
-	const todoComplete = (index) => { 
-		const todoComplete2=(index)=>E
-		let deleteItem=todos[index];
-		console.log(deleteItem)
-		let newTodos=todos.filter((item)=>item!=deleteItem);
-		todos=newTodos;
-	};
-
-	const todoDelete = (index) => { 
-		const todoDelete2=(index)=>E
-		let deleteItem=todos[index];
-		console.log(deleteItem)
-		let newTodos=todos.filter((item)=>item!=deleteItem);
-		todos=newTodos;
-	};
-
 </script>
 <style>
 	.text-font {
@@ -49,26 +42,48 @@ import autoAnimate from "@formkit/auto-animate"
 	}
 	.complete {
 		text-decoration: line-through;
+		text-decoration-thickness: 2px;  
 	}
 </style>
 
 
 <div class="flex space-y-5 flex-col sm:p-0 p-3 min-h-screen items-center justify-center">
-	<div class="flex sm:flex-row flex-col items-center outline-none focus:outline-none ease-linear transition-all duration-150">
-		<input type="text" name="" class="sm:w-80 w-72 mr-1 mb-1 px-3 py-2 cursor-pointer shadow-xl bg-white border-2 rounded text-black text-font" placeholder="Görev Ekle" id="s" bind:value={name2}>
+	<div class="flex sm:flex-row items-center flex-col items-center outline-none focus:outline-none ease-linear transition-all duration-150">
+		<input type="text" name="" class="sm:w-80 w-72 mr-1 mb-1 px-3 py-2 cursor-pointer shadow-xl bg-white border-2 rounded text-black text-font" placeholder="Görev Ekle" id="s"  bind:value={title2}>
 		<button on:click={addTodo} class="w-20 mr-1 mb-1 px-4 py-2 bg-white rounded text-black ursor-pointer shadow-xl border-2 text-font" id="add"><i class="fa-solid fa-plus"></i></button>
+	    {#if todos.length > 0}
+		<button on:click={() => {
+			todos = todos.filter(todo => !todo);
+		}} class="w-20 mr-1 mb-1 px-4 py-2 bg-white rounded text-black ursor-pointer shadow-xl border-2 text-font" id="remove"><i class="fa-solid fa-trash"></i></button>
+		{/if}
+		{#if todos.length > 0}
+		<button on:click={() => {
+			todos = todos.map(todo => {
+				todo.complete = !todo.complete;
+				return todo;
+			});
+		}} class="w-20 mr-1 mb-1 px-4 py-2 bg-white rounded text-black ursor-pointer shadow-xl border-2 text-font" id="complete"><i class="fa-solid fa-check"></i></button>
+		{/if}
 	</div>
 	
-	<div use:autoAnimate class="outline-none focus:outline-none  grid grid-rows-5 ease-linear transition-all duration-150 space-y-2">
-		{#each todos as {id, name, complete}, index}
+	<div use:autoAnimate class="outline-none focus:outline-none  grid sm:grid-rows-2 sm:grid-cols-2 lg:grid-rows-3 lg:grid-cols-3 grid-cols-1 ease-linear transition-all duration-150 gap-3">
+		{#each todos as {id, title, num, complete}, index}
 		<div class="flex outline-none focus:outline-none ease-linear transition-all duration-150 px-4 py-2 bg-white cursor-pointer shadow-xl mr-1 mb-1 items-center justify-between rounded sm:w-80 w-72">
-			<button on:click={ () => todoComplete(index)} class="mr-1 mb-1 px-4 py-1 hover:outline hover:outline-offset-2 hover:outline-green-500 bg-green-500 rounded cursor-pointer shadow-xl text-font text-white outline-none focus:outline-none ease-linear transition-all duration-150"><i class="fa-duotone fa-check"></i></button>
-			<span class="text-font text-black" class:complete={complete}>{index + 1} - {name}</span>
-			<button on:click={ () => todoComplete(index)} class="mr-1 rounded text-font text-red-500 mb-1 px-4 py-2"><i class="fa-solid fa-xmark"></i></button>
+			<button  on:click={ () => completeTodo(id)} class="mr-1 mb-1 px-4 py-1 hover:outline hover:outline-offset-2 hover:outline-green-500 bg-green-500 rounded cursor-pointer shadow-xl text-font text-white outline-none focus:outline-none ease-linear transition-all duration-150"><i class="fa-duotone fa-check"></i></button>
+			<span class="text-font text-black outline-none focus:outline-none ease-linear transition-all duration-150" class:complete={complete}> {title}</span>
+			<button on:click={ () => deleteTodo(id)} class="mt-0.5 rounded text-font text-red-500 text-xl  px-4 py-2"><i class="fa-solid fa-xmark"></i></button>
 		</div>
 		{/each} 	
 	</div>	
-	
+	{#if todos.length === 0}
+	<div class="flex text-center items-center justify-center">
+		<span class="text-font text-black text-xl">Görevleriniz yok</span> 
+	</div>	
+	{/if}
+		<div class="fixed inset-x-0 flex justify-center p-2 bottom-0 w-full">
+			<h1 class="text-font text-sm">Bu site github üzerinden açık kaynaklıdır.</h1>
+		</div>
+		<a href="https://github.com/xDarkShiny/to-do-r" class="fixed top-0 right-5 text-3xl p-2"><i class="fa-brands fa-github"></i></a>
 </div>
 
 	
